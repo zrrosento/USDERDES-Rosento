31be05790cd0aced6cc68ffa580dbf1197e067bd
Zierbyn Prince R. Rosento <52141536+zrrosento@users.noreply.github.com>
2019-09-24 22:00:17 +0800

    Add files via upload

---
+++ Activities8.php
@@ -0,0 +1,177 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Zierbyn Prince Rosento's Website</title>
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+	<script src="https://use.fontawesome.com/releases/v5.0.8/js/all.js"></script>
+	<link href="style.css" rel="stylesheet">
+</head>
+<body>
+
+
+<!-- Navigation -->
+<nav class="navbar navbar-expand-md navbar-light bg-light sticky-top">
+	<div class="container-fluid">
+		<a href="https://www.apc.edu.ph" class="navbar-brand"><img src="test.jpg" alt="Brand Logo"></a>
+		<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive">
+			<span class="navbar-toggler-icon"></span>
+		</button>   
+		<div class="collapse navbar-collapse" id="navbarResponsive">
+			<ul class="navbar-nav ml-auto">
+				<li class="nav-item active"><a href="test1.php" class="nav-link">Home</a></li>
+				<li class="nav-item"><a href="activities.php" class="nav-link">Activities</a></li>
+				<li class="nav-item"><a href="reflections.php" class="nav-link">Reflections</a></li>
+				<li class="nav-item"><a href="quizzes.php" class="nav-link">Quizzes</a></li>
+				<li class="nav-item"><a href="https://www.apc.edu.ph" class="nav-link">About Us</a></li>
+			</ul>
+		</div>
+	</div>
+</nav>
+
+<!---carousel--->
+<div id="slides" class="carousel slide" data-ride="carousel">
+	<div class="carousel-inner">
+		<div class="carousel-item active">
+			<img src="background_act.jpg" alt="image of first slider">
+			<div class="carousel-caption">
+				<h1 class="display-2">Quiz</h1>
+				<p class="h3 subtitle">Q1</p>
+				<button type="button" class="btn btn-outline-light btn-lg" ></a>View All Activiies</button>
+				<button type="button" class="btn btn-primary btn-lg">Back Home</button>
+			</div>
+		</div>
+	</div>
+</div>
+
+<!--- Two Column Section -->
+<div class="container-fluid padding">
+		<div class="row padding">
+			<div class="col-lg-6">
+                <h2>Activity 8: Grid Layout Design</h2>
+                <p>1.</p>
+                <img src="A8_1.png" alt="Bootstrap" class="img-fluid">
+                <p>2.</p>
+                <img src="A8_3.jpg" alt="Bootstrap" class="img-fluid">
+                
+					<a href="activities7.php" class="btn btn-primary">Previous Page</a>
+					<a href="Activities9.php" class="btn btn-primary">Next Page</a>
+			</div>
+			<div class="col-lg-6">
+                <h2> </h2>
+                <p>3.</p>
+                <img src="A8_2.png" alt="Bootstrap" class="img-fluid">
+                <p>4.</p>
+                <img src="A8_4.jpg" alt="Bootstrap" class="img-fluid">
+			</div>		
+		</div>
+	</div>
+	
+
+<hr class="my-4">
+
+
+<!--- Outputs -->
+<div class="container-fluid padding">
+	<div class="row welcome text-center">
+		<div class="col-12">
+			<h2 class="display-4">My Outputs</h2>
+		</div>
+
+	</div>
+</div>
+
+<!--- Cards -->
+<div class="container-fluid padding">
+	<div class="row padding card-deck">
+		<div class="card">
+			<img src="activity.png" alt="Activity" class="card-img-top">
+			<div class="card-body">
+				<h3 class="card-title">Activities</h3>
+				<p class="card-text">This is where you could see the compilation of my Activities.</p>
+				<a href="activities.php" class="btn btn-outline-secondary">View</a>
+			</div>
+		</div>
+		<div class="card">
+			<img src="reflection1.jpg" alt="Picture of Mary Jo" class="card-img-top">
+			<div class="card-body">
+				<h3 class="card-title">Reflections</h3>
+				<p class="card-text">This is where you would be able to read my reflections regarding with our subject and learning.</p>
+				<a href="reflections.php" class="btn btn-outline-secondary">View</a>
+			</div>
+		</div>
+		<div class="card">
+			<img src="quizzes.jpg" alt="Picture of Phil Ho" class="card-img-top">
+			<div class="card-body">
+				<h3 class="card-title">Quizzes</h3>
+				<p class="card-text">You could see a glimpse of what kind of Quizzes do we take and my scores with this subject.</p>
+				<a href="quizzes.php" class="btn btn-outline-secondary">View</a>
+			</div>
+		</div>
+	</div>
+</div>
+
+<hr class="my-4">
+<!--- Connect -->
+<div class="container-fluid padding">
+	<div class="row text-center padding">
+		<div class="col-12">
+			<h2>Connect</h2>
+		</div>
+		<div class="col-12 social padding">
+			<a href="https://www.facebook.com/"><i class="fab fa-facebook"></i></a>
+			<a href="https://twitter.com/"><i class="fab fa-twitter"></i></a>
+			<a href="https://steamcommunity.com/"><i class="fab fa-steam"></i></a>
+			<a href="https://www.instagram.com/"><i class="fab fa-instagram"></i></a>
+			<a href="https://www.youtube.com/"><i class="fab fa-youtube"></i></a>
+		
+		</div>
+	</div>
+</div>
+
+<!--- Footer -->
+<footer>
+		<div class="container-fluid padding">
+			<div class="row text-center">
+	
+				<div class="col-md-4">
+					<hr class="light">
+					<h5>Contact Us</h5>
+					<hr class="light">
+					<p>776-07-05</p>
+					<p>Asia Pacific College</p>
+					<p>#3 Humabon Place, Makati City</p>
+				</div>
+	
+				<div class="col-md-4">
+					<hr class="light">
+					<h5>Hours</h5>
+					<hr class="light">
+					<p>Monday: 7:30am - 9:30pm</p>
+					<p>Thursday: 7:30am - 9:30pm</p>
+					<p>Other days: N/A</p>
+				</div>
+				<div class="col-md-4">
+					<hr class="light">
+					<h5>Supported by:</h5>
+					<hr class="light">
+					<p>Digital Design</p>
+					<p>Asia Pacific College</p>
+					
+				</div>
+				<div class="col-12">
+					<hr class="light-100">
+					<p>&copy; Zierbyn Prince R. Rosento</p>
+				</div>
+			</div>
+		</div>
+	</footer>
+
+
+
+</body>
+</html>
---
+++ activities5.php
@@ -0,0 +1,230 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Zierbyn Prince Rosento's Website</title>
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+	<script src="https://use.fontawesome.com/releases/v5.0.8/js/all.js"></script>
+	<link href="style.css" rel="stylesheet">
+</head>
+<body>
+
+
+<!-- Navigation -->
+<nav class="navbar navbar-expand-md navbar-light bg-light sticky-top">
+	<div class="container-fluid">
+		<a href="https://www.apc.edu.ph" class="navbar-brand"><img src="test.jpg" alt="Brand Logo"></a>
+		<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive">
+			<span class="navbar-toggler-icon"></span>
+		</button>   
+		<div class="collapse navbar-collapse" id="navbarResponsive">
+			<ul class="navbar-nav ml-auto">
+				<li class="nav-item active"><a href="test1.php" class="nav-link">Home</a></li>
+				<li class="nav-item"><a href="activities.php" class="nav-link">Activities</a></li>
+				<li class="nav-item"><a href="reflections.php" class="nav-link">Reflections</a></li>
+				<li class="nav-item"><a href="quizzes.php" class="nav-link">Quizzes</a></li>
+				<li class="nav-item"><a href="https://www.apc.edu.ph" class="nav-link">About Us</a></li>
+			</ul>
+		</div>
+	</div>
+</nav>
+
+<!---carousel--->
+<div id="slides" class="carousel slide" data-ride="carousel">
+	<div class="carousel-inner">
+		<div class="carousel-item active">
+			<img src="background_act.jpg" alt="image of first slider">
+			<div class="carousel-caption">
+				<h1 class="display-2">Quiz</h1>
+				<p class="h3 subtitle">Q1</p>
+				<button type="button" class="btn btn-outline-light btn-lg" ></a>View All Activiies</button>
+				<button type="button" class="btn btn-primary btn-lg">Back Home</button>
+			</div>
+		</div>
+	</div>
+</div>
+
+<!--- Two Column Section -->
+<div class="container-fluid padding">
+	<div class="row padding">
+		<div class="col-lg-6">
+			<h2>Activity 5: Initial Prototype - Log In Page</h2>
+            <h1>Bass, Julian Terry </h1>
+            <h1>Fortuno, Adrian</h1>
+            <h1>Rosento, Prince</h1>
+            <h1>Corpuz, Michael</h1>
+		
+        <div class="col-lg-6">
+			<img src="project1.png" alt="woman sitting on a desk" class="img-fluid">
+			<p>Figure 1. The Initial Landing Page of the APC Game Lab Reservation System. Note the options to sign in via an existing Google or Office account, or to create one from scratch via a personal email. There's also an option to login with an existing account.</p>
+		</div>
+		<div class="col-lg-6">
+			<img src="project2.png" alt="woman sitting on a desk" class="img-fluid">
+			<p>Figure 2. The same menu for mobile with the navbar button added on the top-left</p>
+		</div>
+		<div class="col-lg-6">
+			<img src="project3.png" alt="woman sitting on a desk" class="img-fluid">
+			<p>Figure 3. Navbar displayed</p>
+		</div>
+		<div class="col-lg-6">
+			<img src="project4.png" alt="woman sitting on a desk" class="img-fluid">
+			<p>Figure 4. This page shows the option to sign in with one's Office account. The primary details concerned are the school email and password, as well as a "forgot password" option.</p>
+		</div>
+		<div class="col-lg-6">
+			<img src="project5.png" alt="woman sitting on a desk" class="img-fluid">
+			<p>Figure 5. Errors for Invalid office Email and/or Password</p>
+		</div>
+		<div class="col-lg-6">
+			<img src="project6.png" alt="woman sitting on a desk" class="img-fluid">
+			<p>Figure 6. Office Sign In for Mobile</p>
+		</div>
+		<div class="col-lg-6">
+			<img src="Cproject7.png" alt="woman sitting on a desk" class="img-fluid">
+			<p>Figure 7. Office Email and Password errors for Mobile</p>
+		</div>
+		<div class="col-lg-6">
+			<img src="project8.png" alt="woman sitting on a desk" class="img-fluid">
+			<p>Figure 8. This page shows the option to sign up with a preferred personal email (gmail, yahoo, etc.), and has a redirecting link to the login option in the case that an account has already been made. Note the required field to re-confirm the password.</p>
+		</div>
+		<div class="col-lg-6">
+			<img src="project9.png" alt="woman sitting on a desk" class="img-fluid">
+			<p>Figure 9. Errors for non-existent or taken emails, and weak passwords</p>
+		</div>
+		<div class="col-lg-6">
+			<img src="project10.png" alt="woman sitting on a desk" class="img-fluid">
+			<p>Figure 10. Signing up with personnal email on Mobile</p>
+		</div>
+		<div class="col-lg-6">
+			<img src="project11.png" alt="woman sitting on a desk" class="img-fluid">
+			<p>Figure 11. Account creation errors on Mobile</p>
+		</div>
+		<div class="col-lg-6">
+			<img src="project12.png" alt="woman sitting on a desk" class="img-fluid">
+			<p>Figure 12. This page shows the option to sign in with a previously made account other than that concerning the google and office options. User may use full name or email. Note the redirecting link in case an account has not been made yet.</p>
+		</div>
+		<div class="col-lg-6">
+			<img src="project13.png" alt="woman sitting on a desk" class="img-fluid">
+			<p>Figure 12. Errors for invalid emails/names/passwords</p>
+		</div>
+		<div class="col-lg-6">
+			<img src="project14.png" alt="woman sitting on a desk" class="img-fluid">
+			<p>Figure 14. Signing in with previously created account on Mobile.</p>
+		</div>
+		<div class="col-lg-6">
+			<img src="project15.png" alt="woman sitting on a desk" class="img-fluid">
+			<p>Figure 15. Errors for invalid email/name/password on Mobile.</p>
+		</div>
+			<br>
+			<a href="activities4.php" class="btn btn-primary">Previous Page</a>
+            <a href="activities6.php" class="btn btn-primary">Next Page</a>
+		</div>
+		
+	</div>
+</div>
+
+<hr class="my-4">
+
+<!--- Outputs -->
+<div class="container-fluid padding">
+	<div class="row welcome text-center">
+		<div class="col-12">
+			<h2 class="display-4">My Outputs</h2>
+		</div>
+
+	</div>
+</div>
+
+<!--- Cards -->
+<div class="container-fluid padding">
+	<div class="row padding card-deck">
+		<div class="card">
+			<img src="activity.png" alt="Activity" class="card-img-top">
+			<div class="card-body">
+				<h3 class="card-title">Activities</h3>
+				<p class="card-text">This is where you could see the compilation of my Activities.</p>
+				<a href="activities.php" class="btn btn-outline-secondary">View</a>
+			</div>
+		</div>
+		<div class="card">
+			<img src="reflection1.jpg" alt="Picture of Mary Jo" class="card-img-top">
+			<div class="card-body">
+				<h3 class="card-title">Reflections</h3>
+				<p class="card-text">This is where you would be able to read my reflections regarding with our subject and learning.</p>
+				<a href="reflections.php" class="btn btn-outline-secondary">View</a>
+			</div>
+		</div>
+		<div class="card">
+			<img src="quizzes.jpg" alt="Picture of Phil Ho" class="card-img-top">
+			<div class="card-body">
+				<h3 class="card-title">Quizzes</h3>
+				<p class="card-text">You could see a glimpse of what kind of Quizzes do we take and my scores with this subject.</p>
+				<a href="quizzes.php" class="btn btn-outline-secondary">View</a>
+			</div>
+		</div>
+	</div>
+</div>
+
+<hr class="my-4">
+<!--- Connect -->
+<div class="container-fluid padding">
+	<div class="row text-center padding">
+		<div class="col-12">
+			<h2>Connect</h2>
+		</div>
+		<div class="col-12 social padding">
+			<a href="https://www.facebook.com/"><i class="fab fa-facebook"></i></a>
+			<a href="https://twitter.com/"><i class="fab fa-twitter"></i></a>
+			<a href="https://steamcommunity.com/"><i class="fab fa-steam"></i></a>
+			<a href="https://www.instagram.com/"><i class="fab fa-instagram"></i></a>
+			<a href="https://www.youtube.com/"><i class="fab fa-youtube"></i></a>
+		
+		</div>
+	</div>
+</div>
+
+<!--- Footer -->
+<footer>
+		<div class="container-fluid padding">
+			<div class="row text-center">
+	
+				<div class="col-md-4">
+					<hr class="light">
+					<h5>Contact Us</h5>
+					<hr class="light">
+					<p>776-07-05</p>
+					<p>Asia Pacific College</p>
+					<p>#3 Humabon Place, Makati City</p>
+				</div>
+	
+				<div class="col-md-4">
+					<hr class="light">
+					<h5>Hours</h5>
+					<hr class="light">
+					<p>Monday: 7:30am - 9:30pm</p>
+					<p>Thursday: 7:30am - 9:30pm</p>
+					<p>Other days: N/A</p>
+				</div>
+				<div class="col-md-4">
+					<hr class="light">
+					<h5>Supported by:</h5>
+					<hr class="light">
+					<p>Digital Design</p>
+					<p>Asia Pacific College</p>
+					
+				</div>
+				<div class="col-12">
+					<hr class="light-100">
+					<p>&copy; Zierbyn Prince R. Rosento</p>
+				</div>
+			</div>
+		</div>
+	</footer>
+
+
+
+</body>
+</html>
